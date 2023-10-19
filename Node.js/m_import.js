5f40d4819ad924f67bc0696a361e03602e40185d
DsBrito <dionatas.ufes@gmail.com>
2023-10-18 21:57:23 -0300

    ⚡ add :: node import/export

--- Node.js/m_import.js
+++ Node.js/m_import.js
@@ -9,4 +9,6 @@ console.log(add_sub.add(18, 20));
 console.log(add_sub.sub(18, 20));
 
 console.log("\n" + "- using object import by requiere -");
-console.log(mensage)
+console.log(mensage);
+
+console.log("\n" + "- using variable import by requiere -");
