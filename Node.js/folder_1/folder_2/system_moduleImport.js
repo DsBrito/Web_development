7a4724e352510d975d605ffdf8cd3aa9c26dbd31
DsBrito <dionatas.ufes@gmail.com>
2023-10-20 11:38:26 -0300

    ⚡ add :: node import/export

---
+++ Node.js/folder_1/folder_2/system_moduleImport.js
@@ -0,0 +1,21 @@
+//forms of export modules inside folder
+
+
+const module_1 = require("./../../mensage_export.js");
+const module_2 = require("./../../function_export.js");
+
+console.log("\n" + "- using ./../../mensage_export.js");
+console.log(module_1.hello);
+console.log(module_1.welcome);
+console.log(module_1.bye);
+
+console.log("\n" + "- using ./../../function_export.js");
+console.log(module_2.add(5, 5));
+console.log(module_2.sub(1, 15));
+
+//require http module
+const http = require("http");
+http.createServer((req, res) => {
+    res.write('Hello im a http server :D');
+    res.end();
+}).listen(8082);
